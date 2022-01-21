fc2ac52669b1bb4898d31cfedb02b0b0b86d8a81
Lakpa Sange Sherpa <sangesherpa1215@gmail.com>
2022-01-21 10:10:03 +0545

    OOP 2nd commit

--- OOP/intro.php
+++ OOP/intro.php
@@ -1,18 +1,36 @@
 <?php
 
-class Introduction {
-    // var $x = 10;
-    public $x = 10;
+// class Introduction {
+//     // var $x = 10;
+//     public $x = 10;
 
-    function test() {
-        return "the number is: {$this->x}";
+//     function test() {
+//         return "the number is: {$this->x}";
+//     }
+// }
+
+// $obj = new Introduction();
+
+// // -> object operator
+// // :: static method
+// echo $obj->x;
+
+// // PDO - Php Data Object
+
+class Introduction {
+
+    public function __construct($name, $age) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        echo $this->name;
+    }
+
+    public function __destruct() {
+        echo "This is __destruct.";
     }
 }
 
-$obj = new Introduction();
-
-// -> object operator
-// :: static method
-echo $obj->x;
-
-// PDO - Php Data Object
+$obj = new Introduction("Mike", 10);
+$obj->getName();
